760c7a48a7d726a0ff9747708a6d75529ab13fae
imshockwaves <42camillecharrier@gmail.com>
2023-07-24 18:24:31 +0200

    🔧 Add eslint and prettier

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,62 +1,62 @@
-import dotenv from 'dotenv';
-import fastify from 'fastify';
+import dotenv from "dotenv";
+import fastify from "fastify";
 import blippPlugin from "fastify-blipp";
 dotenv.config();
-import cors from '@fastify/cors'
+import cors from "@fastify/cors";
 import path from "path";
-import * as Knex from 'knex';
+import * as Knex from "knex";
 
 export const knex = Knex.knex({
-  client: 'mysql',
-  connection: {
-    host : process.env.DATABASE_HOST,
-    port : 3306,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
-  }
+	client: "mysql",
+	connection: {
+		host: process.env.DATABASE_HOST,
+		port: 3306,
+		user: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_NAME,
+	},
 });
 
 const port = "3000";
 export const app = fastify({
-  logger: true
-})
+	logger: true,
+});
 
 app.register(require("@fastify/jwt"), {
-  secret: "supersecret"
-})
+	secret: "supersecret",
+});
 
-app.decorate("authenticate", async function(request: any, reply: any) {
-  try {
-    await request.jwtVerify()
-  } catch (err) {
-    reply.send(err)
-  }
-})
+app.decorate("authenticate", async function (request: any, reply: any) {
+	try {
+		await request.jwtVerify();
+	} catch (err) {
+		reply.send(err);
+	}
+});
 
-app.register(require('@fastify/static'), {
-  root: path.join(__dirname, 'assets'),
-  prefix: '/assets/', // optional: default '/'
-})
+app.register(require("@fastify/static"), {
+	root: path.join(__dirname, "assets"),
+	prefix: "/assets/", // optional: default '/'
+});
 
-app.register(blippPlugin)
-app.register(import('./routes/pokemon.routes'), { prefix: 'api/v1/pokemon' })
-app.register(import('./routes/user.routes'), { prefix: 'api/v1/user' })
-app.register(import('./routes/pokelist.routes'), { prefix: 'api/v1/pokelist' })
-app.register(import('./routes/generation.routes'), { prefix: 'api/v1/generation' })
+app.register(blippPlugin);
+app.register(import("./routes/pokemon.routes"), { prefix: "api/v1/pokemon" });
+app.register(import("./routes/user.routes"), { prefix: "api/v1/user" });
+app.register(import("./routes/pokelist.routes"), { prefix: "api/v1/pokelist" });
+app.register(import("./routes/generation.routes"), { prefix: "api/v1/generation" });
 app.register(cors, {
-  // put your options here
-})
+	// put your options here
+});
 
 async function main() {
-  // Run the server!
-  try {
-    await app.listen({ port: 3000 })
-    app.blipp();
-  } catch (err) {
-    app.log.error(err)
-    process.exit(1)
-  }
+	// Run the server!
+	try {
+		await app.listen({ port: 3000 });
+		app.blipp();
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 }
 
 main();
